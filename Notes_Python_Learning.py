76944527cb6c31f6294bd82708fd1d3f1c1b3895
Julio Gomes <julio@pointec.dev>
2022-11-11 23:42:36 -0300

    new content for study

---
+++ Notes_Python_Learning.py
@@ -0,0 +1,164 @@
+"""
+Title: Python Lessons
+Developer: Júlio Gomes
+Date created: 11/2022
+"""
+
+''' First Step print with comments '''
+
+# Print a Text
+# \r\n -> CRLF
+# \n -> LF
+print("Test", 1, "Delimitator", sep="-", end='\nEnd of line\n')
+
+#Notation in a string using ''
+print("Notation 'in' a string")
+
+#Type in a string
+print(type(12),type(1.2),type("abc"),type(True))
+
+#Boolean type in a string
+print(10 == 10)
+print(type(False == False))
+
+#Converting numbers and strings
+print(float('1') + 1)
+print(str(11) + 'b')
+
+#Using variables
+varInt = 4 #is a variable integer
+varStr = 'Jacob' #is a variable string
+varBool = varInt <= 18 #is a variable boolean
+print("User:",varStr, varInt,"Years - Underage:",varBool, sep=" ",end=".\n")
+
+#Calculating and using input
+var1 = input('Digite n1: ')
+var2 = input('Digite n2: ')
+n1 = int(var1)
+n2 = int(var2)
+print("Sum", n1+n2,"Subtract",n1-n2,"Multiply",n1*n2,"Divide",n1/n2, sep=" ")
+print("Int Division", n1//n2,"Exponentiation",n1**n2,"Module",n1%n2, sep=" ")
+
+#Concat String and Multiply5
+n1 = 5
+n2 = "python"
+print((str(n1)+n2)*n1)
+print(n2 * n1)
+
+#Ord                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ering Calculation functions
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
+
+#Layout for print
+nome = "Name"
+idade = 23
+formato = f'{nome} have {idade:.2f} years old'
+print(formato)
+
+#If statement
+idade0 = input('Type an age ')
+idade = int(idade0)
+if idade < 18:
+    print('Underage')
+elif idade >=18:
+    print('Majority age')
+else:
+    print('Format input not allowed.')
+
+#Relational operators
+var1 = input('Digite n1: ')
+var2 = input('Digite n2: ')
+n1 = int(var1)
+n2 = int(var2)
+print('n1 diferente n2 ',n1 != n2)
+print('n1 igual n2 ',n1 == n2)
+print(n1>n2 and n2!=n1)
+print(n1>n2 or n2!=n1)
+print(not n2!=n1)
+
+#Bool using OR statement - 1 true and 0 false
+variavel_a = 1 or 0
+variavel_b = 0 or 1
+print(bool(1), bool(0))
+
+#In Statement
+nome =input('digite nome ')
+if 'o' in nome:
+    print(f'O nome {nome} usa o.')
+else:
+    print(f'O nome {nome} NÃO usa o.')
+
+#Not In Statement
+nome =input('digite nome ')
+if 'o' not in nome:
+    print(f'O nome {nome} sem uso do o.')
+else:
+    print(f'O nome {nome} não pode usar o.')
+
+#Interpolation for strings
+"""
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
+
+#Formating a string
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+
+#Split the string
+var1 = 'a simple text'
+print(var1[2:9:1])
+#Invert the string
+var1 = 'a simple text'
+print(var1[::-1])
+
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+numero_str = input(
+    'Vou dobrar o número que vc digitar: '
+)
+
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+
+#Is, Not Is and None
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if is None)
+
+#replace value in a variable
+nome = "joao da silva"
+outra_variavel = nome
+outra_variavel = f'{outra_variavel[:3]}ABC{outra_variavel[4:]}'
+print(outra_variavel)
---
+++ teste.py
@@ -0,0 +1,93 @@
+
+# #Exercicio de Cadastro
+# print("Cadastro\n")
+# nome = input("Nome: ")
+# idade = input("Idade: ")
+
+# if nome != "":
+#     intidade = int(idade)
+
+# validationForm = ((nome != "") and (idade != "")) 
+# if validationForm:
+#     print(f"Nome: {str(nome)}")
+#     print(f"Idade: {str(idade)}")
+#     print(f"nome invertido: {nome[::-1]}")
+
+#     if ' ' in nome:
+#         print(f'O nome {nome} usa Espaço.')
+#     else:
+#         print(f'O nome {nome} NÃO usa Espaço.')
+    
+#     print("o nome contém " + str(len(nome)) + " letras")
+#     print("primeira letra " + str(nome[1]))
+#     print("ultima letra " + str(nome[-1]))
+
+# else:
+#     print("campo faltando")
+
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+# print("Avaliando número")
+# varInt = input("Digite um número inteiro: ")
+
+# varIntValidate = (str.isdigit(varInt))
+
+# if varIntValidate:
+#     print("It's a Integer number")
+#     if (int(varInt)%2) == 0:
+#         print("It's an even number")
+#     else:
+#         print("It's an odd number")
+# else:
+#     print("It's not a Integer number")
+    
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+# import re
+# print("Programa de saudação")
+# varHoras = input("Digite as horas: ")
+
+# r = re.compile('.*:.*')
+# if not r.match(varHoras) is not None:
+#    print ('error: invalid format time')
+#    exit()
+
+# varHorasHoras = int(varHoras[0:2:1])
+
+# if varHorasHoras >= 0 and varHorasHoras <=11:
+#     print(f"Good Morning is {varHoras} o clock")
+# elif varHorasHoras >= 12 and varHorasHoras <=17:
+#     print(f"Good Afternoon is {varHoras} o clock")
+# elif varHorasHoras >= 18 and varHorasHoras <=23:
+#     print(f"Good Evening is {varHoras} o clock")
+# else:
+#     print("Time value not allowed")
+
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+# print("Medidor de nome")
+# nome = input("digite o nome: ")
+
+# curto = len(nome) <=4
+# normal = len(nome) >= 5 and len(nome) <=6
+# grande  = len(nome) >= 7
+
+# if curto:
+#     print(f"O nome {nome} é curto com {len(nome)} letras")
+# if normal:
+#     print(f"O nome {nome} é normal com {len(nome)} letras")
+# if grande:
+#     print(f"O nome {nome} é grande com {len(nome)} letras")
+
+
